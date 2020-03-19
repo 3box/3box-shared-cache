50286b1323c2b6fd64a9692898dafbbc5b3e6631
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2020-03-19 14:00:13 +0100

    test: use in memory storage and test with log db

--- test/client.orbitdb.test.js
+++ test/client.orbitdb.test.js
@@ -1,54 +1,68 @@
 const { createClient } = require('../src')
+const { getClientOpts, startRpcServer } = require('./testUtils')
 
 const OrbitDbStorageAdapter = require('orbit-db-storage-adapter')
+const OdbKeystore = require('orbit-db-keystore')
 const OrbitDbCache = require('orbit-db-cache')
 const OrbitDB = require('orbit-db')
 const IPFS = require('ipfs')
 
 describe("Shared cache module", () => {  
-  let ClientStore, db, backendDb = {}
+  let ipfs, orbitDb, ClientStore, db, rpcOpts = {
+    db: {}
+  }
   
   beforeAll(
     async () => {
+      console.log('this is run multiple times?')
       const clientOpts = getClientOpts()
-      
-      startRpcServer({ backendDb })
-
       ClientStore = createClient(clientOpts)
 
+      startRpcServer(rpcOpts)
+
       const cachePath = './orbitdb/cache'
+
       const orbitDbStorage = OrbitDbStorageAdapter(
         (...args) => new ClientStore(...args),
         {}
       )
-
       const cacheStorage = await orbitDbStorage.createStore(cachePath)
       const cache = new OrbitDbCache(cacheStorage)
 
-      const ipfs = await IPFS.create()
-      const orbitDb = await OrbitDB.createInstance(ipfs, { cache })
+      const keystorePath = './orbitdb/keystore'
+      const odbStorage = await OrbitDbStorageAdapter((...args) => new ClientStore(...args), {})
+      const keyStorage = await odbStorage.createStore(keystorePath)
+      const keystore = new OdbKeystore(keyStorage)
       
-      db = await orbitDb.log('0')
+      ipfs = await IPFS.create()
+
+      orbitDb = await OrbitDB.createInstance(ipfs, { cache, keystore, storage: odbStorage })
+      
+      db = await orbitDb.log('news')
     }
   )
 
   afterAll(
-    () => {
-      Object.keys(backendDb).forEach(
-        (k) => delete backendDb[k]
+    async () => {
+      Object.keys(rpcOpts.db).forEach(
+        (k) => delete rpcOpts.db[k]
       )
-
-      ipfs.stop()
+      
+      await db.close()
+      await orbitDb.disconnect()
+      await ipfs.stop()
     }
   )
 
   describe("ClientStore against object store through orbit-db as cache", () => {
-    it('creates cache on new entry: add', async () => {
-      await db.add('1')
-      console.log({ backendDb})
+    it.only('updates cache on database instantiation', async () => {
+      const hash = await db.add('hello')
       expect(
-        Object.keys(backendDb).length
-      ).toEqual(2)
-    })
+        JSON.parse(
+          rpcOpts.db[`${db.id}/_localHeads`]
+        )[0].payload.value
+      )
+        .toEqual('hello')
+      })
   })
 })
